cd750dd568a1f4f2429c89647d443020e7134a0e
Phoebe He <33295821+phxwhe@users.noreply.github.com>
2023-01-04 14:57:38 -0800

    Update streamlit_app.py
    format and display json file

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,7 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# normalize json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display
+streamlit.dataframe(fruityvice_normalized)
